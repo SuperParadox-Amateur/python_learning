31060fa146378b3edd8dc77a512ade6749de16fb
SuperParadox-Amateur <1521012642@qq.com>
2021-11-17 18:05:30 +0800

    Add new files

--- pywebapp.py
+++ pywebapp.py
@@ -1,15 +1,8 @@
-from pywebio.input import *
+from pywebio import *
 
-age = input("How old are you?", type=NUMBER)
+def main(): # PyWebIO application function
+    name = input.input("what's your name")
+    output.put_text("hello", name)
 
-password = input("Input password.", type = PASSWORD)
 
-gift = select("Which gift you want?", ["keyboard", "ipad"])
-
-agree = checkbox("User Term", options=["I agree to term and conditions"])
-
-answer = radio("Choose one", options=["A", "B", "C", "D"])
-
-text = textarea("Text Area", rows=3, placeholder="Some text")
-
-image = file_upload("Select a image: ", accept="image/*")
+start_server(main, port=8080, debug=True)
---
+++ secondapp.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("FQ304大修数据展示")
+
+f_path = "C:/Users/Dell/Documents/FQ304大修人员剂量明细.xlsx"
+
+def get_data():
+    data = pd.read_excel(
+        f_path,
+        
+    )
+
+    return data
+
+data = get_data()
+
+st.table(data)
